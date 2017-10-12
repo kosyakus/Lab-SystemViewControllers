65647d984cb6d8a100e170b8de311ebb5f1e12c2
Unknown <kosyak_@mail.ru>
2017-10-13 01:51:25 +0300

    created access to users camera or photo library

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import SafariServices // to display web content
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -32,10 +33,57 @@ class ViewController: UIViewController {
         present(activityController, animated: true, completion: nil)
     }
     
-    @IBAction func safariButtonTapped(_ sender: Any) {
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        // create url from string, create instance of SFS and present SafariVC
+        if let url = URL(string: "http://apple.com") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated:  true, completion: nil)
+        }
+        
     }
-    @IBAction func photoButtonTapped(_ sender: Any) {
+    
+    // to access the user's camera or photo library two protocols must be adopted: UIImagePickerControllerDelegate and UINavigationControllerDelegate
+    // also in Info.plist enter a new key for NSPhotoLibraryUsageDescription (Privacy - Photo Library Usage), for value enter smth like "To share photos from the camera or photo library"
+    @IBAction func photoButtonTapped(_ sender: UIButton) {
+        
+        let imagePicker = UIImagePickerController() // instance
+        imagePicker.delegate = self // set this VC as delegate
+        
+        // ask the user if they want to take a new photo or select an existing photo from library
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(cameraAction)
+        }
+        
+        /*let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in
+            print("User selected Camera action")
+        })*/
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "photo library", style: .default, handler: { action in
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+        /*let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+            print("User selected Photo Library action")
+        })*/
+        
+        alertController.popoverPresentationController?.sourceView = sender
+        
+        present(alertController, animated: true, completion: nil)
+        
     }
+    
     @IBAction func emailButtonTapped(_ sender: Any) {
     }
     
