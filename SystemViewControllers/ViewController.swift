90c57ec0a8646f6196e98a1faedde5073fafe05a
Admin <kosyak_@mail.ru>
2017-10-12 15:33:12 +0300

    Initial Commit

---
+++ SystemViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Admin on 12.10.17.
+//  Copyright © 2017 NS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
