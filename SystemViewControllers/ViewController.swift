bca0eea09e24bc2a38f45c65f37c18113a3aa369
Unknown <kosyak_@mail.ru>
2017-10-12 15:55:38 +0300

    Created outlets and actions for system VC

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        
+        guard let image = imageView.image else {return}
+        
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender
+        
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: Any) {
+    }
+    @IBAction func photoButtonTapped(_ sender: Any) {
+    }
+    @IBAction func emailButtonTapped(_ sender: Any) {
+    }
+    
 }
 
